7990601071cdfc3714870f386f2ee8768cf901f4
emduc386-stack <emduc386@gmail.com>
2026-08-02 20:34:03 +0700

    Update love.js

--- data/love.js
+++ data/love.js
@@ -1,1 +1,22 @@
+const loveMessages = [
 
+"Yêu bản thân là khởi đầu của mọi tình yêu.",
+
+"Một lời quan tâm nhỏ cũng có thể làm ấm một ngày.",
+
+"Đừng vội từ bỏ khi còn yêu thương.",
+
+"Người phù hợp sẽ đến vào đúng thời điểm.",
+
+"Tình yêu cần sự chân thành hơn là hoàn hảo.",
+
+"Tha thứ đôi khi là món quà cho chính mình.",
+
+"Một trái tim biết yêu luôn mạnh mẽ.",
+
+"Hãy trân trọng những người luôn ở bên bạn.",
+
+"Nụ cười là ngôn ngữ đẹp nhất của tình yêu.",
+
+"Đôi khi yêu thương chỉ là lắng nghe."
+];
