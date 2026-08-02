c70ab6dbc5dab5131383930845ffeacae8b472c3
emduc386-stack <emduc386@gmail.com>
2026-08-02 12:33:54 +0700

    Create messages.js

---
+++ data/messages.js
@@ -0,0 +1,12 @@
+const messages = [
+  "Hôm nay là một cơ hội mới.",
+  "Đừng so sánh mình với người khác.",
+  "Tiến chậm vẫn tốt hơn đứng yên.",
+  "Mọi khó khăn rồi sẽ qua.",
+  "Hãy tin vào bản thân.",
+  "Mỗi ngày hãy học một điều mới.",
+  "Sự kiên trì luôn có giá trị.",
+  "Bạn mạnh mẽ hơn bạn nghĩ.",
+  "Thất bại chỉ là bài học.",
+  "Đừng quên mỉm cười hôm nay."
+];
