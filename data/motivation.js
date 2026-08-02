18333ff6b17941356c0e2ae6a9b0ecd5f00ec86b
emduc386-stack <emduc386@gmail.com>
2026-08-02 20:34:35 +0700

    Update motivation.js

--- data/motivation.js
+++ data/motivation.js
@@ -1,1 +1,22 @@
+const motivationMessages = [
 
+"Đừng bỏ cuộc khi còn có thể cố gắng.",
+
+"Mỗi ngày tiến một bước vẫn là tiến.",
+
+"Ước mơ cần hành động để thành hiện thực.",
+
+"Kiên trì sẽ tạo nên khác biệt.",
+
+"Bạn mạnh mẽ hơn bạn nghĩ.",
+
+"Thất bại chỉ là một bài học.",
+
+"Hãy bắt đầu ngay hôm nay.",
+
+"Sự chăm chỉ luôn được đền đáp.",
+
+"Không ai thành công chỉ sau một đêm.",
+
+"Tin vào chính mình."
+];
