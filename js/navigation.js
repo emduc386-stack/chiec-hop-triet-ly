b42134d96228ccabfaad33faa23439dd994ce4c7
emduc386-stack <emduc386@gmail.com>
2026-08-04 21:29:37 +0700

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -1,1 +1,55 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    const path = window.location.pathname;
+
+    let prefix = "";
+
+    if(path.includes("/pages/")){
+        prefix = "../";
+    }
+
+    const page = path.split("/").pop();
+
+    let active = "home";
+
+    if(page === "choose-box.html"){
+        active = "box";
+    }else if(page === "history.html" || page === "history-detail.html"){
+        active = "history";
+    }else if(page === "setting.html"){
+        active = "setting";
+    }
+
+    document.body.insertAdjacentHTML("beforeend",`
+
+<div class="bottomNav">
+
+<a href="${prefix}index.html"
+class="navItem ${active==="home"?"active":""}">
+<span>🏠</span>
+Trang chủ
+</a>
+
+<a href="${prefix}pages/choose-box.html"
+class="navItem ${active==="box"?"active":""}">
+<span>📦</span>
+Hộp
+</a>
+
+<a href="${prefix}pages/history.html"
+class="navItem ${active==="history"?"active":""}">
+<span>📖</span>
+Lịch sử
+</a>
+
+<a href="#"
+class="navItem ${active==="setting"?"active":""}">
+<span>⚙️</span>
+Cài đặt
+</a>
+
+</div>
+
+`);
+
+});
